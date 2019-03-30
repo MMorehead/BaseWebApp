f601bfcee3c0343d765faa9393627796d3f4b8bd
Margaret Morehead <mmorehead@Margarets-MacBook-Pro.local>
2019-03-30 14:51:57 -0400

    Database Application

--- project/js/main.js
+++ project/js/main.js
@@ -7,5 +7,49 @@ function myFunction() {
 
   // 'img-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
   $("#doge-image").append(`<img class="img-circle" src="images/wowdoge.jpeg" />`);
+
+    
 }
+
+// Gets called whenever the user clicks "sign in" or "sign out".
+function toggleSignIn() {
+  if (!firebase.auth().currentUser) { // if the user's not logged in, handle login
+    var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/plus.login');
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      console.log("success");
+    }).catch(function(error) {
+      console.error("error", error);
+    });
+  } else { // handle logout
+    firebase.auth().signOut();
+  }
+  //This disables the button until login or logout is successful. You'll want to replace 'login-button' with the id of your login button.
+  $("#login").attr("disabled", true);
+}
+
+// Collects the datan from the input fields
+function handleMessageFormSubmit() {
+  var title = $("#title").val();
+  var body = $("#message").val();
+  console.log(title);
+  console.log(body);
+
+  addMessage(body, title);
+
+}
+
+// sends the data to the database
+function addMessage(body, title) {
+  var postData = {
+    title: title,
+    body: body
+  };
+  var newPostKey = firebase.database().ref().child('stream').push().key;
+  firebase.database().ref('/stream/' + newPostKey).set(postData);
+
+}
+
+
+
 
